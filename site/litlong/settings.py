f841ea8010a6f2cf6dc7830c3e03ed4ed35abd6c
Ian Fieldhouse <me@ianfieldhouse.com>
2015-03-12 12:15:05 +0000

    Alter LANGUAGE_CODE and TIME_ZONE to reflect UK

--- site/litlong/settings.py
+++ site/litlong/settings.py
@@ -75,9 +75,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
