f4c4ceb7eb3dcdc80c343ef53d9146a664fb6107
Ian Fieldhouse <me@ianfieldhouse.com>
2015-03-12 15:48:55 +0000

    Create migration to load POS fixture data

---
+++ site/api/migrations/0009_load_pos_fixture_data.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from django.db import models, migrations
+from django.core.management import call_command
+
+app_name = 'api'
+fixture = model_name = 'PartOfSpeech'
+
+
+def load_fixture(apps, schema_editor):
+    call_command('loaddata', fixture, app_label=app_name)
+
+
+def unload_fixture(apps, schema_editor):
+    "Deleting all entries for this model"
+
+    PartOfSpeech = apps.get_model(api, model_name)
+    PartOfSpeech.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        (app_name, '0008_posmention'),
+     ]
+
+    operations = [
+        migrations.RunPython(load_fixture, reverse_code=unload_fixture),
+    ]
