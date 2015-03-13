bb3a41a58de9a684970c91d635bdda93b72484fe
Ian Fieldhouse <me@ianfieldhouse.com>
2015-03-13 13:23:23 +0000

    Expose Collection to Document relationship in API

--- site/api/serializers.py
+++ site/api/serializers.py
@@ -3,14 +3,16 @@ from api.models import Collection, Document
 
 
 class CollectionSerializer(serializers.HyperlinkedModelSerializer):
+    documents = serializers.HyperlinkedRelatedField(many=True, read_only=True,
+            view_name='document-detail')
 
     class Meta:
         model = Collection
-        fields = ('text', 'url',)
+        fields = ('url', 'text', 'documents',)
 
 
 class DocumentSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Document
-        fields = ('title', 'pubdate', 'author', 'external_url', 'collection', 'url',)
+        fields = ('url', 'title', 'pubdate', 'author', 'external_url', 'collection',)
