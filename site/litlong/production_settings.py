c1aae3f7c1d990f8c7ec58dc22802a63d0838f1d
Ian Fieldhouse <me@ianfieldhouse.com>
2015-03-24 12:32:40 +0000

    Create production WSGI and settings files

---
+++ site/litlong/production_settings.py
@@ -0,0 +1,25 @@
+import os
+from settings import *
+from YamJam import yamjam
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+CFG = yamjam(os.path.join(PROJECT_PATH, 'etc/yamjam/config.yaml'))['litlong']
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+SECRET_KEY = CFG['secret-key']
+
+DATABASES = {
+    'default': {
+        'ENGINE': CFG['database']['engine'],
+        'NAME': CFG['database']['name'],
+        'USER': CFG['database']['username'],
+        'PASSWORD': CFG['database']['password'],
+        'HOST': CFG['database']['host'],
+        'PORT': CFG['database']['port'],
+    }
+}
+
+STATIC_URL = '/static/'
