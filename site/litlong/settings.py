8875ab662532f0de8421c386807a7531f989b418
Ian Fieldhouse <me@ianfieldhouse.com>
2015-03-24 12:25:55 +0000

    Split settings out into local and base settings

--- site/litlong/settings.py
+++ site/litlong/settings.py
@@ -1,35 +1,11 @@
 """
 Django settings for litlong project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from YamJam import yamjam
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_PATH = os.path.abspath(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-
-CFG = yamjam(os.path.join(PROJECT_PATH, 'etc/yamjam/config.yaml'))['litlong']
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = CFG['secret-key']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
 
 # Application definition
@@ -61,24 +37,6 @@ ROOT_URLCONF = 'litlong.urls'
 
 WSGI_APPLICATION = 'litlong.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': CFG['database']['engine'],
-        'NAME': CFG['database']['name'],
-        'USER': CFG['database']['username'],
-        'PASSWORD': CFG['database']['password'],
-        'HOST': CFG['database']['host'],
-        'PORT': CFG['database']['port'],
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-GB'
 
 TIME_ZONE = 'Europe/London'
@@ -88,13 +46,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
-
 
 REST_FRAMEWORK = {
 # Use Django's standard `django.contrib.auth` permissions,
