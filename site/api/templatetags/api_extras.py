6cc5cd75d9e680f59851b11adcbeada6c23545d2
Ian Fieldhouse <me@ianfieldhouse.com>
2015-03-25 11:13:23 +0000

    Add highlighting of search terms to snippets

--- site/api/templatetags/api_extras.py
+++ site/api/templatetags/api_extras.py
@@ -10,6 +10,15 @@ register = template.Library()
 @stringfilter
 def wrap_with_span(string, arg):
     """ Wraps all instances of a string with a span element"""
-    return re.sub(r'\b{0}\b'.format(re.escape(arg)),
-                  '<span>{0}</span>'.format(arg), string).replace(
-                          '&amp;#x', '&#x')
+
+    words = arg.split(' ')
+
+    for word in words:
+        pattern = re.compile(r'(\b{0}[\w\d]*\b)'.format(word), flags=re.I)
+
+        for (match) in re.findall(pattern, string):
+            string = re.sub(r'{0}'.format(match),
+                '<span>{0}</span>'.format(match), string)
+
+    return string.replace('&amp;#x', '&#x')
+
